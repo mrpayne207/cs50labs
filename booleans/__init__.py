06f683ddfd23f82c7f5fc91cd799f1c8452605b9
Kyle Vitale <53164742+KV-CS@users.noreply.github.com>
2021-08-05 10:05:25 -0700

    Add files via upload

---
+++ booleans/__init__.py
@@ -0,0 +1,14 @@
+import check50
+import check50.c
+
+filename = "booleans.c"
+@check50.check()
+def exists():
+    """%s exists.""" % filename
+    check50.exists(filename)
+
+@check50.check(exists)
+def compiles():
+    """%s compiles.""" % filename
+    check50.c.compile(filename, lcs50=True)
+
