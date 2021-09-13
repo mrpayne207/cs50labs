77d154e507d0a1dfad78a34b3191ab9550cc97a1
mrpayne207 <54715609+mrpayne207@users.noreply.github.com>
2021-09-13 16:07:57 -0500

    Create __init__.py

---
+++ conditions/__init__.py
@@ -0,0 +1,13 @@
+import check50
+import check50.c
+
+filename = "conditions.c"
+@check50.check()
+def exists():
+    """%s exists.""" % filename
+    check50.exists(filename)
+
+@check50.check(exists)
+def compiles():
+    """%s compiles.""" % filename
+    check50.c.compile(filename, lcs50=True)
