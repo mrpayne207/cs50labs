1b330833da680976215e4e7aae74f77714c12fc0
Kyle Vitale <53164742+KV-CS@users.noreply.github.com>
2021-10-08 17:18:10 -0700

    Create functions2.c

---
+++ functions/functions2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <cs50.h>
+
+//Function Prototype to tell compiler function exists 
+long squared_sums(int a, int b);
+
+int main (void)
+{
+  //Prompt user to enter two numbers
+  
+  
+  
+  //Call the squared_sums function with those numbers as arguments
+  //Remember, the function should return a valeue, so you'll need a variable 
+  //to store that in.
+  
+  
+  
+  //Print the result
+  
+  
+}
+
+//Function definition
+long squared_sums(int a, int b)
+{
+  int sum;
+  
+  //Square each int, a and b
+  
+  
+  //Now add them together and store in sum variable
+  
+  
+  return sum;
+  
+}
