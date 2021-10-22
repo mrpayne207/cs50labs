88dc3dc75b320357a6f74c45693b5086f2d0ac48
Kyle Vitale <53164742+KV-CS@users.noreply.github.com>
2021-10-21 21:57:06 -0700

    Create __init__.py

---
+++ arrays/__init__.py
@@ -0,0 +1,24 @@
+import check50
+import check50.c
+
+filename = "array.c"
+@check50.check()
+def array_exists():
+    """%s exists.""" % filename
+    check50.exists(filename)
+
+@check50.check(array_exists)
+def compiles():
+    """%s compiles.""" % filename
+    check50.c.compile(filename, lcs50=True)
+
+filename2 = "string.c"
+@check50.check()
+def string_exists():
+    """%s exists.""" % filename2
+    check50.exists(filename2)
+
+@check50.check(string_exists)
+def compiles():
+    """%s compiles.""" % filename2
+    check50.c.compile(filename2, lcs50=True)
