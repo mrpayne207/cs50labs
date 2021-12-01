55d7ca0410e14d7e5f4925985cf8c46a38d43e92
Kyle Vitale <53164742+KV-CS@users.noreply.github.com>
2021-12-01 15:43:07 -0800

    Create __init__.py

---
+++ structs/__init__.py
@@ -0,0 +1,24 @@
+import check50
+import check50.c
+
+filename = "mystruct.c"
+@check50.check()
+def mystruct_exists():
+    """%s exists.""" % filename
+    check50.exists(filename)
+
+@check50.check(mystruct_exists)
+def compiles():
+    """%s compiles.""" % filename
+    check50.c.compile(filename, lcs50=True)
+
+filename2 = "struct1.c"
+@check50.check()
+def struct1_exists():
+    """%s exists.""" % filename2
+    check50.exists(filename2)
+
+@check50.check(struct1_exists)
+def string_compiles():
+    """%s compiles.""" % filename2
+    check50.c.compile(filename2, lcs50=True)
